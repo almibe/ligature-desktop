db49a3e5219efdbffcc7dbae2db6930aef7cd2cd
berry3 <berry3@indiana.edu>
2021-06-28 22:39:30 -0400

    adding new deps + clean up to ligature-lab, #38

--- packages/ligature-lab/src/components/dataset/LigatureDataset.svelte
+++ packages/ligature-lab/src/components/dataset/LigatureDataset.svelte
@@ -13,7 +13,3 @@
 <div class="row">
     <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => addStatement()}>Add Statement</button></div>
 </div>
-<div class="row">
-    <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => editDataset(dataset)}>Add Statement</button></div>
-    <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => editDataset(dataset)}>Add Statement</button></div>
-</div>
