479b5242144bd72f1904700e429901da438a6c8d
berry3 <berry3@indiana.edu>
2021-07-23 22:34:26 -0400

    renaming unit test to nothing + starting to fill in parser, #20

--- packages/wander/lib/index.ts
+++ packages/wander/lib/index.ts
@@ -2,8 +2,90 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { createToken, CstParser, Lexer } from 'chevrotain';
+import { WHITE_SPACE_T,
+    ANGLE_START_T,
+    ATTRIBUTE_START_T,
+    ANGLE_END_T,
+    IDENTIFIER_T,
+    STRING_T,
+    BYTES_T,
+    FLOAT_T,
+    INTEGER_T
+} from '@ligature/lig';
+
+const COMMENT_T = createToken({ name: "Comment", pattern: /#.*\n/, group: Lexer.SKIPPED });
+
+const allTokens = [
+    COMMENT_T,
+    WHITE_SPACE_T,
+    ANGLE_START_T,
+    ATTRIBUTE_START_T,
+    ANGLE_END_T,
+    IDENTIFIER_T,
+    STRING_T,
+    BYTES_T,
+    FLOAT_T,
+    INTEGER_T
+];
+
+class WanderParser extends CstParser {
+    constructor() {
+        super(allTokens);
+
+        const $ = this;
+
+        $.RULE("statements", () => {
+            $.MANY(() => {
+                $.SUBRULE($.statement);
+            })
+        });
+
+        $.RULE("statement", () => {
+            $.SUBRULE($.entity);
+            $.SUBRULE($.attribute);
+            $.SUBRULE($.value);
+            $.SUBRULE2($.entity);
+        });
+
+        $.RULE("entity", () => {
+            $.CONSUME(ANGLE_START_T);
+            $.CONSUME(IDENTIFIER_T);
+            $.CONSUME(ANGLE_END_T);
+        });
+
+        $.RULE("attribute", () => {
+            $.CONSUME(ATTRIBUTE_START_T);
+            $.CONSUME(IDENTIFIER_T);
+            $.CONSUME(ANGLE_END_T);
+        });
+
+        $.RULE("value", () => {
+            $.OR([
+                { ALT: () => $.SUBRULE($.entity) },
+                { ALT: () => $.CONSUME(STRING_T) },
+                { ALT: () => $.CONSUME(FLOAT_T) },
+                { ALT: () => $.CONSUME(INTEGER_T) },
+                { ALT: () => $.CONSUME(BYTES_T) }
+            ])
+        });
+
+        this.performSelfAnalysis();
+    }
+
+    //properties below just exist to make TS happy
+    entity: any;
+    attribute: any;
+    value: any;
+    statement: any;
+    statements: any;
+}
+
+let wanderLexer = new Lexer(allTokens);
+let wanderParser = new WanderParser();
+
 export class WanderInterpreter {
-    run(script: String): any {
-        throw new Error("Not implemented.");
+    run(script: string): any {
+        return "nothing";
     }
 }
