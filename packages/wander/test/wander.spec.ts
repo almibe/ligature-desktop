2619d5752b88fe5476413ef7415bab3a1fa76047
berry3 <berry3@indiana.edu>
2021-08-01 11:21:15 -0400

    replacing jest with mocha+chai, closes #76

--- packages/wander/test/wander.spec.ts
+++ packages/wander/test/wander.spec.ts
@@ -2,26 +2,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { expect } from 'chai';
 import { readdirSync, readFileSync } from 'fs';
+import { debug } from '../lib/debug';
 import { WanderInterpreter, write } from '../lib/index';
 import { ast } from './ASTs';
 
 //add wander files names below when you only want to run a set number of tests, leave empty to test all
-const runOnly: Array<string> = ['entity-err.wander'];
+const runOnly: Array<string> = ['let-res.wander'];
 
 describe('Wander AST tests', () => {
     const wander = new WanderInterpreter();    
     readdirSync(__dirname + "/resources").forEach(dir => {
         readdirSync(__dirname + "/resources/" + dir).forEach(testFile => {
             if (testFile.endsWith("wander") && (runOnly.length == 0 || runOnly.indexOf(testFile) != -1)) {
-                test(testFile, () => {
+                it(testFile, () => {
                     const script = readFileSync(__dirname + "/resources/" + dir + "/" + testFile);
                     const result = wander.createAst(script.toString());
                     const expected = ast[testFile];
                     if (testFile.includes('err')) {
-                        expect(result).toHaveProperty("type", "wanderError");
+                        expect(result).to.have.property("type", "wanderError");
                     } else {
-                        expect(result).toEqual(expected);
+                        debug(result);
+                        expect(result).to.be.eql(expected);                        
                     }
                 })
             }
@@ -34,11 +37,11 @@ describe('Wander eval tests', () => {
     readdirSync(__dirname + "/resources").forEach(dir => {
         readdirSync(__dirname + "/resources/" + dir).forEach(testFile => {
             if (testFile.endsWith("wander") && (runOnly.length == 0 || runOnly.indexOf(testFile) != -1)) {
-                test(testFile, () => {
+                it(testFile, () => {
                     const script = readFileSync(__dirname + "/resources/" + dir + "/" + testFile);
                     const expected = readFileSync(__dirname + "/resources/" + dir + "/" + testFile.replace(/\.wander$/, ".result"));
                     const result = wander.run(script.toString());
-                    expect(write(result)).toMatch(expected.toString());
+                    expect(write(result)).to.be.eql(expected.toString());
                 })
             }
         });
