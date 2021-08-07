e5395965936317f8450f106b07426fc5fc881028
berry3 <berry3@indiana.edu>
2021-08-07 16:30:23 -0400

    adding not function to stdlib, #84

--- packages/wander/lib/stdlib.ts
+++ packages/wander/lib/stdlib.ts
@@ -2,12 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { WanderValue } from ".";
+import { Identifier, NativeFunction } from "./ast";
 import { Binding } from "./binding";
 
 export function stdLib(): Binding {
     const stdLib = new Binding();
 
-    //TODO add functions
+    stdLib.bind(new Identifier("not"), new NativeFunction(["bool"], (parameters: WanderValue[]) => {
+        let p = parameters[0] as boolean
+        return !p
+    }));
 
     return stdLib;
 }
