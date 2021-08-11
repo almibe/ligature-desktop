5bff6047c86ac1b43172b936f133693731fd3ab2
berry3 <berry3@indiana.edu>
2021-08-11 18:13:54 -0400

    starting to remove sveltekit code, #89

--- packages/ligature-lab/src/components/dataset/WanderPanel.svelte
+++ packages/ligature-lab/src/components/dataset/WanderPanel.svelte
@@ -5,6 +5,7 @@
     import { checkValue } from "./dataset";
     import {defaultKeymap} from "@codemirror/commands"
     import {EditorState, EditorView, basicSetup} from "@codemirror/basic-setup"
+    import { WanderInterpreter } from '@ligature/wander/lib/index'
 
     export let dataset: Dataset
 
@@ -19,14 +20,33 @@
         messages = [...messages, message];
     }
 
+    function clearMessages() {
+        messages = []
+    }
+
+    function setMessage(message: string) {
+        messages = [message]
+    }
+
     onMount(() => {
         let view = new EditorView({
             state: EditorState.create({extensions: [basicSetup]}),
             parent: document.getElementById("editor")
         })
     });
+
+    let interpreter = new WanderInterpreter()
+
+    function runScript() {
+        addMessage("Running");
+    }
 </script>
 
+<div class="row">
+    <div class="col">
+        <button type="button" class="btn btn-outline-dark" on:click={() => runScript()}>Run</button>
+    </div>
+</div>
 <div class="row align-items-center">
     <div class="col">
         <div id="editor" />
