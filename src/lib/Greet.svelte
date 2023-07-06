73abc1442c545fe2bde36c54bf5e2d000a64002f
Alex Michael Berry <alexmiberry@gmail.com>
2023-07-06 04:03:19 -0400

    getting app to work with svelte again, #143

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri"
 
-  let name = "";
+  let input = "";
   let greetMsg = ""
 
   async function greet(){
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    greetMsg = await invoke("greet", { input })
   }
 </script>
 
 <div>
   <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <input id="greet-input" placeholder="Enter a name..." bind:value={input} />
     <button type="submit">Greet</button>
   </form>
   <p>{greetMsg}</p>
