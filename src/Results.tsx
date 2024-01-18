677e6e5455ca86185a881f32232f0ec71c817cdb
Alex Michael Berry <alexmiberry@gmail.com>
2024-01-18 04:54:13 -0500

    applets working, closes #155 using solid stores, closes #160

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,40 +1,27 @@
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
-import '@shoelace-style/shoelace/dist/components/alert/alert.js';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
 import '@shoelace-style/shoelace/dist/components/menu/menu.js';
 import '@shoelace-style/shoelace/dist/components/menu-item/menu-item.js';
 import '@shoelace-style/shoelace/dist/components/divider/divider.js';
+import '@shoelace-style/shoelace/dist/components/card/card.js';
 import { bus } from './bus.ts';
-import { For, createSignal } from 'solid-js';
-import { ModuleValue, WanderError, WanderResult, WanderValue } from '@wander-lang/wander/src/values.ts';
-import { Either } from 'purify-ts'
-import { printResult, printValue } from '@wander-lang/wander/src/interpreter.ts';
-
-export interface Applet {
-    readonly name: string
-    readonly predicate: (value: WanderResult) => boolean
-    readonly render: (value: WanderResult) => Element
-}
+import { For } from 'solid-js';
+import { WanderResult } from '@wander-lang/wander/src/values.ts';
+import { createStore } from "solid-js/store";
+import { produce } from "solid-js/store";
+import { Applet, applets, rawTextApplet } from './applets.ts';
+const [store, setStore] = createStore({results: []});
 
 interface Result {
     readonly id: number
     readonly script: string
     readonly wanderResult: WanderResult
     readonly applet: Applet
+    readonly content: string
 }
-
-const [results, setResults] = createSignal<Result[]>([]);
-const [applets, setApplets] = createSignal<Applet[]>([]);
 
 let id = 0n;
-
-const textApplet: Applet = {
-    name: "Text",
-    predicate: (value) => true,
-    render: (value: WanderResult) => {
-        return <span class="result">{printResult(value)}</span>
-    }
-}
 
 bus.on("AddResult", (wanderResult: WanderResult) => {
     id++;
@@ -42,31 +29,23 @@ bus.on("AddResult", (wanderResult: WanderResult) => {
         id,
         script: "?",
         wanderResult: wanderResult,
-        applet: textApplet
-    }
-    setResults([result, ...results()])
+        applet: rawTextApplet,
+        content: rawTextApplet.render(wanderResult)
+    };
+    setStore(produce((store) => {
+        store.results = [result, ...store.results];
+    }))
 })
 
 bus.on("RemoveResult", (result) => {
-    console.log(result)
-    const newResult = results().filter(r => r.id != result.id);
-    setResults(newResult);
+    setStore(produce((store) => {
+        store.results = store.results.filter(r => r.id != result.id);
+    }))
 })
-
-bus.on("AddApplet", (applet: Applet) => {
-    setApplets([applet, ...applets()])
-})
-
-bus.on("RemoveApplet", (applet: Applet) => {
-      const newResult = applets().filter(r => r == applet);
-      setApplets(newResult);
-})
-
-bus.emit("AddApplet", textApplet)
 
 export function Results() {
     return <>
-        <For each={results()}>
+        <For each={store.results}>
             {(result) =>
                 <Result result={result} />
             }
@@ -76,50 +55,42 @@ export function Results() {
 
 function Result(props: { result: Result}) {
     return <div class="result">
-        {props.result.wanderResult.isLeft() && <ResultError error={props.result.wanderResult.leftOrDefault("")}></ResultError>}
-        {props.result.wanderResult.isRight() && <ResultSuccess result={props.result}></ResultSuccess> }
+        {renderResult(props.result)}
     </div>
 }
 
-function ResultSuccess(props: { result: Result}) {
-    if (props.result.wanderResult.isLeft()) {
-        renderResult("danger", printResult(props.result.wanderResult), props.result.id, props.result)
-    } else {
-        const result: WanderValue = props.result.wanderResult.unsafeCoerce()[0];
-        if (result.type == "Module") {
-            if (result.value.has("error")) {
-                return renderResult("danger", result.value.get("error").value, props.result.id, props.result)
-            } else {
-                return renderResult("success", printValue(result.value.get("result")), props.result.id, props.result)
-            }
-        } else {
-            return renderResult("success", printValue(result), props.result.id, props.result)
-        }
-    }
+function changeApplet(result: Result, applet: Applet) {
+    setStore(produce((store) => {
+        let prevResult = store.results.find(r => r.id == result.id)
+        prevResult.applet = applet
+        prevResult.content = applet.render(result.wanderResult)
+    }))
 }
 
-function renderResult(variant: string, message: string, id: number, result: Result) {
-    return <sl-alert variant={variant} open>
-        {result.applet.render(result.wanderResult)}
+function renderResult(result: Result) {
+    const filteredApplets = () => applets().filter(applet =>
+        applet.predicate(result.wanderResult) 
+    )
+
+    return <sl-card style="width:100%">
+        <span innerHTML={result.content}></span>
         <sl-dropdown style="float:right">
-            <sl-icon slot="trigger"  name="caret" library="system"></sl-icon>
-            <sl-menu>
-                <For each={applets()}>
+            <sl-button slot="trigger">
+               {result.applet.name}<sl-icon name="caret" library="system"></sl-icon>
+            </sl-button>
+            <sl-menu id={"menu" + result.id}>
+                <For each={filteredApplets()}>
                     {(applet) =>
-                        <sl-menu-item >{applet.name}</sl-menu-item>
+                        <sl-menu-item onClick={() => changeApplet(result, applet)} >{applet.name}</sl-menu-item>
                     }
                 </For>
                 <sl-divider></sl-divider>
-                <sl-menu-item >
-                    <span onClick={e => bus.emit("RemoveResult", {id})} style="float:right">
+                <sl-menu-item>
+                    <span onClick={e => bus.emit("RemoveResult", {id: result.id})} style="float:right">
                         Remove <sl-icon name="x-lg" library="system"></sl-icon>
                     </span>
                 </sl-menu-item>
             </sl-menu>
         </sl-dropdown>
-    </sl-alert>
-}
-
-function ResultError(props) {
-    return <h1>Error</h1>;
+    </sl-card>
 }
