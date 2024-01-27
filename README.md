377fa4806d7b1155703867e7d5f23f975643d4ed
Alex Michael Berry <alexmiberry@gmail.com>
2024-01-27 04:22:00 -0500

    removing wander-shell code, closes #166

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# WanderShell
+# WanderDesktop
+
+*Note: this project is on hiatus at the moment.* 
+*See https://github.com/almibe/wander-ts for active development related to this project.*
 
 See https://tauri.app/v1/guides/getting-started/prerequisites for setting up a development environment.
 Once this has been setup run the following.
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { ShellPanel } from "./ShellPanel.tsx";
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import '@shoelace-style/shoelace/dist/components/button/button.js';
-import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js';
-import './interpreter.ts'
-
 function App() {
   return (
       <div>
-        <ShellPanel />
+        !?
       </div>
   );
 }
