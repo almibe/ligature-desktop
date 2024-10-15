f34d7a96372e956365a76e4283daecc6653546fe
Alex Michael Berry <alexmiberry@gmail.com>
2024-10-15 00:01:22 -0400

    adding tauri 2 code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,25 +4,7 @@ import { StoreProvider } from './StoreProvider.tsx';
 import Split from 'split-grid'
 
 export function App() {
-
-  setTimeout(() => {
-    Split({
-      columnGutters: [{
-        track: 1,
-        element: document.querySelector('.gutter'),
-      }],
-    })  
-  })
-
   return <StoreProvider>
-    <div id="split-panel">
-      <div>
-        <Networks></Networks>
-      </div>
-      <div class="gutter"></div>
-      <div>
-        <Wander></Wander>
-      </div>
-    </div>
+      <Wander></Wander>
   </StoreProvider>
 }
