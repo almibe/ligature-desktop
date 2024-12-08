ee3d255f5a50314a5a3e666608c74621659317bd
Alex Michael Berry <alexmiberry@gmail.com>
2024-12-08 05:11:53 -0500

    fixing split pane

--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,14 +1,18 @@
 import { onMount } from "solid-js"
-import Split from "split.js"
 import { showEditor } from "@ligature/ligature-components/src/editor/editor"
 import { run as runScript } from "@ligature/ligature"
 import { EditorView } from "codemirror";
-
+import Split from 'split-grid'
 
 export function Application() {
     let editor: EditorView = null;
     onMount(() => {
-        Split(["#top", "#results"], { direction: "vertical"})
+        Split({
+            rowGutters: [{
+                track: 1,
+                element: document.querySelector('.gutter-row-1'),
+            }]
+        })
         editor = showEditor("#editor")
     })
 
@@ -17,14 +21,17 @@ export function Application() {
     }
     
     return <div style="height:100%; width: 100%">
-        <div id="top">
-            <div>
-                <button onclick={run}><img src="../../static/icons/play.svg" alt="Add" /></button>
+        <div class="grid" style="height:100%; width: 100%">
+            <div id="top" style="overflow: auto">
+                <div>
+                    <button onclick={run}><img src="../../static/icons/play.svg" alt="Add" /></button>
+                </div>
+                <div id="editor"></div>
             </div>
-            <div id="editor"></div>
-        </div>
-        <div id="results">
-            Results
+            <div class="gutter-row gutter-row-1"></div>
+            <div id="results" style="height:100%; width: 100%">
+                Results
+            </div>
         </div>
     </div>
 }
