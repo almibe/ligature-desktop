f436d207ed35ccbadc02cc22664006378927c50f
Alex Michael Berry <alexmiberry@gmail.com>
2025-04-27 18:08:04 -0400

    starting to use shoelace

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,37 +1,43 @@
 <script lang="ts">
   import { runScript, showEditor } from '@ligature/ligature-components'
+  import './style.css'
+  import { onMount } from 'svelte';
+  import '@shoelace-style/shoelace/dist/shoelace.js'
+  import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js'
+  import '@shoelace-style/shoelace/dist/themes/light.css'
 
-import './style.css'
-    import { onMount } from 'svelte';
-
-// let initalScript = `
-// { display canvas 
-//   width 200 
-//   height 200 
-//   instructions [
-//     [fill-style blue]
-//     [fill-rect 10 10 150 100]]}`
-
-onMount(() => {
-  let initalScript = `
-{ display table 
+  onMount(() => {
+    let initalScript = 
+`{ display table 
   network (assertions [a b c] [a d e] [w e e])}`
 
-let editor = showEditor(document.querySelector("#editor"), initalScript)
+    let editor = showEditor(document.querySelector("#editor"), initalScript)
 
-document.querySelector("#runButton")?.addEventListener("click", () => {
-    document.querySelector("#results").innerHTML = ""
-    runScript(editor.state.doc.toString(), document.querySelector("#results"))
-})
-
-})
-
+    document.querySelector("#runButton")?.addEventListener("click", () => {
+      document.querySelector("#results").innerHTML = ""
+      runScript(editor.state.doc.toString(), document.querySelector("#results"))
+    })
+  })
 </script>
 
 <main class="container">
-  <div><button id="runButton">Run</button></div>
-  <div id="editor"></div>
-  <div id="results"></div>
+
+  <sl-split-panel position="25">
+    <div
+      slot="start"
+      style=""
+    >
+      <div id="sidebar">Sidebar</div>
+    </div>
+    <div
+      slot="end"
+      style=""
+    >
+      <div><button id="runButton">Run</button></div>
+      <div id="editor"></div>
+      <div id="results"></div>
+    </div>
+  </sl-split-panel>
 </main>
 
 <style>
