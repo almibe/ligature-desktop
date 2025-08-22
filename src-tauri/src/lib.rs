627754770e220737891b78f6626faae5231946ac
Alex Michael Berry <alexmiberry@gmail.com>
2025-08-21 21:54:30 -0400

    starting to use dialogs updating ui/removing tree

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,13 +4,22 @@ use walkdir::WalkDir;
 
 #[tauri::command]
 fn start_up(webview_window: tauri::WebviewWindow) -> () {
-    let test_suite = env::var("LIGATURE_TEST_SUITE").unwrap() + "/core/";
-    for entry in WalkDir::new(&test_suite) {
-        let entry = entry.unwrap();
-        if entry.path().extension() == Some(OsStr::new("wander")) {
-            webview_window.emit("add_file", entry.path().display().to_string().trim_start_matches(&test_suite)).unwrap();
-        }
-    }
+    // let test_suite = env::var("LIGATURE_TEST_SUITE").unwrap() + "/core/";
+    // for entry in WalkDir::new(&test_suite) {
+    //     let entry = entry.unwrap();
+    //     if entry.path().extension() == Some(OsStr::new("wander")) {
+    //         webview_window
+    //             .emit(
+    //                 "add_file",
+    //                 entry
+    //                     .path()
+    //                     .display()
+    //                     .to_string()
+    //                     .trim_start_matches(&test_suite),
+    //             )
+    //             .unwrap();
+    //     }
+    // }
 }
 
 #[tauri::command]
@@ -22,10 +31,10 @@ fn open_file(name: &str, webview_window: tauri::WebviewWindow) -> () {
     webview_window.emit("set_editor", contents).unwrap();
 }
 
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![start_up, open_file])
         .run(tauri::generate_context!())
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,68 @@
 <script lang="ts">
-  import { runScript, showEditor } from '@ligature/ligature-components'
+  import { printResult, runScript, showEditor } from '@ligature/ligature-components'
   import './style.css'
   import { onMount } from 'svelte';
   import '@shoelace-style/shoelace/dist/shoelace.js'
   import '@shoelace-style/shoelace/dist/components/split-panel/split-panel.js'
+  import '@shoelace-style/shoelace/dist/components/select/select.js'
   import '@shoelace-style/shoelace/dist/themes/light.css'
-  import SideBar from '../components/SideBar.svelte';
   import { invoke } from '@tauri-apps/api/core';
   import { listen } from '@tauri-apps/api/event';
-  
-  let files = $state([])
+  import { event, window, path } from '@tauri-apps/api'
+  import { save, open } from '@tauri-apps/plugin-dialog';
+
+  let fileName: string | null = $state(null)
+
+  let editor: any | null = null;
+
+  $effect(() => {
+    if (fileName == null) {
+      window.getCurrentWindow().setTitle("Ligature Desktop - New Script")
+    } else {
+      window.getCurrentWindow().setTitle("Ligature Desktop - " + fileName)
+    }
+	});
+
+  function newScript() {
+    fileName = null
+    document.querySelector("#result").innerHTML = ""
+    document.querySelector("#output").innerHTML = ""
+    editor.setValue("")
+  }
+
+  function run() {
+      document.querySelector("#result").innerHTML = ""
+      document.querySelector("#output").innerHTML = ""
+      let res = printResult(runScript(editor.getValue(), document.querySelector("#output")))
+      document.querySelector("#result").innerHTML = res
+  }
+
+  async function openScript() {
+    const selectedFile = await open({
+      multiple: false,
+      directory: false,
+    });
+    fileName = selectedFile
+    console.log(selectedFile);
+  }
+
+  async function saveScript() {
+
+  }
+
+  async function saveAsScript() {
+    const path = await save({
+      filters: [
+        {
+          name: 'Wander Files',
+          extensions: ['wander'],
+        },
+      ],
+    });
+    console.log(path);
+  }
 
   onMount(() => {
-    listen(
-      'add_file',
-      (event) => {
-        files.push(event.payload)
-      }
-    );
-
     listen(
       'set_editor',
       (event) => {
@@ -26,53 +70,42 @@
       }
     )
 
-    let initalScript = `(docs)`
-
-    let editor = showEditor(document.querySelector("#editor"), initalScript)
-
-    document.querySelector("#runButton")?.addEventListener("click", () => {
-      document.querySelector("#results").innerHTML = ""
-      runScript(editor.getValue(), document.querySelector("#results"))
-    })
+    editor = showEditor(document.querySelector("#editor"))
 
     invoke("start_up")
   })
 </script>
 
-<sl-split-panel position="25" class="main">
-  <div
-    slot="start"
-    style="overflow: scroll"
-  >
-    {#each files as file}
-      <div><sl-button variant="text" size="medium" on:click={() => {
-        invoke("open_file", {name: file})
-      }}>{file}</sl-button></div>
-    {/each}
-  </div>
-  <div
-    slot="end"
-    style="overflow: scroll"
-  >
-    <sl-split-panel class="main" position="65" vertical>
-      <div
-        slot="start"
-        style="overflow: scroll"
-      >
-      <div id="topPanel">
-        <div><sl-button id="runButton">Run</sl-button></div>
-        <div id="editor"></div>
-      </div>
-    </div>
-      <div
-        slot="end"
-        style="overflow: scroll"
-      >
-        <div id="results"></div>
-      </div>
-    </sl-split-panel>
-    </div>
-</sl-split-panel>
+<div id="main">
+  <sl-button-group>
+    <sl-button id="newButton" onclick={newScript}>New</sl-button>
+    <sl-button id="openButton" onclick={openScript}>Open</sl-button>
+    <sl-button id="saveButton" onclick={saveScript}>Save</sl-button>
+    <sl-button id="saveAsButton" onclick={saveAsScript}>Save As</sl-button>
+  </sl-button-group>
+  <sl-button-group>
+    <sl-button id="runButton" onclick={run}>Run</sl-button>
+  </sl-button-group>
+
+      <sl-split-panel class="main" position="65" vertical>
+        <div
+          slot="start"
+          style="overflow: scroll"
+        >
+          <div id="topPanel">
+            <div id="editor">docs()</div>
+          </div>
+        </div>
+        <div
+          slot="end"
+          style="overflow: scroll"
+        >
+          <div id="output"></div>
+          <pre><code id="result"></code></pre>
+        </div>
+      </sl-split-panel>
+
+</div>
 
 <style>
 </style>
